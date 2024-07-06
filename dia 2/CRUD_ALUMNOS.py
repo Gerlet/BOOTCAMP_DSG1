b0ec29886fb323d4d158a92ee678164dab766f4e
elcoleccionista28gc@gmail.com <elcoleccionista28gc@gmail.co>
2024-07-06 16:15:47 -0500

    CRUD

--- dia 2/CRUD_ALUMNOS.py
+++ dia 2/CRUD_ALUMNOS.py
@@ -1,25 +1,34 @@
-import tabulate
 import os
+import tabulate
 import time
+lista_alumnos = [
+    {
+        'nombre' : 'Gerardo Grande',
+        'email' : 'elcoleccionista28gc@gmail.com',
+        'celular' : '983762478',
+    }
+
+]
 
 ancho=50
-opc=0
-while(opc<5):
-    os.system("cls")
+opcion=0
+
+while (opcion<5):
+    os.system("clear")
     print("="*ancho)
-    print(" "*10 +"CRUD DE ALUMNOS")
+    print(" "*10 + "CRUD DE ALUMNOS")
     print("="*ancho)
     print("""
 
-          [1] REGISTRAR ALUMNO
-          [2] MOSTRAR ALUMNOS
-          [3] ACTUALIZAR ALUMNO
-          [4] ELIMINAR ALUMNO
-          [5] SALIR
+        [1] REGISTRAR ALUMNO
+        [2] MOSTRAR ALUMNOS
+        [3] ACTUALIZAR ALUMNO
+        [4] ELIMINAR ALUMNO
+        [5] SALIR  
+          
           """)
     print("="*ancho)
-    opc=int(input("ingrese una opción por favor: "))
+    opcion=int(input("INGRESE OPCIÓN: "))
     os.system("cls")
-    print(f"selecciono la opción {opc}")
-    time.sleep(3)
-
+    print(f"Selecciona una opción {opcion}")
+    time.sleep(2)
