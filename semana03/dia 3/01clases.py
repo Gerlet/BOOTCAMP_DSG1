5e30f0a3bbad1701530a40ce817deb25123094ad
elcoleccionista28gc@gmail.com <elcoleccionista28gc@gmail.co>
2024-07-08 21:46:12 -0500

    ok

--- semana03/dia 3/01clases.py
+++ semana03/dia 3/01clases.py
@@ -1,22 +1,40 @@
-class Usuario:
-    __email='elcoleccionista28gc@gmail.com'
-    __pasword='hola1234'
+class Persona:
+    def __init__(self,nom,ema):
+        self.nombre=nom
+        self.email=ema
 
-    def __init__(self):
-        pass
+    def mostrar(self):
+        print("="*50)
+        print(f"Nombre : {self.nombre}")
+        print(f"Email : {self.email}")
 
-    def login(self,email,pasword):
-        if(self.__email==email and self.__pasword == pasword):
-            print(f"Bienvenido {self.__email}")
+
+class Alumno(Persona):
+    def __init__(self,nom,ema,nota):
+        super().__init__(nom,ema)
+        self.nota=nota
+    
+    def mostrar(self):
+        super().mostrar()
+        print(f"Nota : {self.nota}")
+class Profesor(Persona):
+     def __init__(self,nom,ema,esp):
+        super().__init__(nom,ema)
+        self.especialidad=esp
+
+     def mostrar(self):
+         super().mostrar()
+         print(f"Especialidad : {self.especialidad}")
+    
+class Empleado(Persona):
+    pass
         
-        else:
-            print('datos incorrectos')
+alumno1=Alumno('gerardo','gerardo28@gmail.com',15)
+alumno1.mostrar()
+print(f"Nota : {alumno1.nota}")
+profesor1=Profesor('cesar','cesar@gmail.com','base de datos')
+profesor1.mostrar()
+print(f"Especialidad : {profesor1.especialidad}")
 
-print("Login de usuarios")
-email=input("Ingresa email: ")
-pasword=input("Ingrese pasword: ")
-
-usuario=Usuario()
-#print(usuario.pasword)
-#usuario.pasword="123"
-usuario.login(email,pasword)
+empleado1=Empleado('Ana','ana@gmail.com')
+empleado1.mostrar()
