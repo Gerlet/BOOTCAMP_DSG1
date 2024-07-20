f80d5dd3e56414b81faa13e1fa6a24d073b49ea4
elcoleccionista28gc@gmail.com <elcoleccionista28gc@gmail.co>
2024-07-19 22:17:41 -0500

    avance

---
+++ Semana04/dia_3/1-scrapper_sbs.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?geoId=102927786&keywords="
+
+def get_offers(skill):
+    url = requests.get(LINKEDIN_URL+skill)
+
+    if(url.status_code == 200):
+        html = BeautifulSoup(url.text,'html.parser')
+        #with open('linkedin.html','w',encoding='utf-8') as file:
+        #    file.write(url.text)
+        #print(html)
+
+        ul_offers = html.find('ul',{'class':'jobs-search__results-list'})
+        li_offers = ul_offers.find_all('li')
+        #print(li_offers)
+        
+        for offer in li_offers:
+            offer_title = offer.find('h3',{'class':'base-search-card__title'})
+            offer_location = offer.find('span',{'class':'job-search-card__location'})
+            offer_company = offer.find('a')
+            offer_url = offer_company['href']
+            print(f'titulo : {offer_title.get_text().strip()}')
+            print(f'ubicacion : {offer_location.get_text().strip()}')
+            print(f'empresa : {offer_company.get_text().strip()}')
+            print(f'url:{offer_url}')
+    else:
+        print(f"error : {url.status_code}")
+
+if __name__ == '__main__':
+    skill = input('ingrese skill a buscar : ')
+    get_offers(skill)
