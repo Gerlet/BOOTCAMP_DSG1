6403b9a63a91484b039fa967103dc7dc8d0659ec
elcoleccionista28gc@gmail.com <elcoleccionista28gc@gmail.co>
2024-07-22 20:52:28 -0500

    primera_fase

--- semana05/dia_1/et/src/main.py
+++ semana05/dia_1/et/src/main.py
@@ -1,9 +1,19 @@
-from prefect import flow 
+from prefect import flow
 from tasks.task import (task_primera_tarea)
+from tasks.task_extract_linkedin import (
+    task_extract_linkedin)
+from tasks.task_load_offers import (
+    task_load_offers
+)
 
 @flow(name="PRIMER FLOW")
 def main_flow():
-    task_primera_tarea()
-
-if __name__=="__main__":
+    #task_primera_tarea()
+    search = ['python','java','go']
+    for s in search:
+        offers = task_extract_linkedin(s)
+        print(f"se encontraron {offers.__len__()} ofertas de {s}")
+        task_load_offers(offers)
+    
+if __name__ == "__main__":
     main_flow()
