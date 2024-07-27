e61bfbfb21d93608c8e59c3b21cb15b3d2ae1505
elcoleccionista28gc@gmail.com <elcoleccionista28gc@gmail.co>
2024-07-26 19:48:45 -0500

    go

--- semana05/dia_2/01-prueba-apiperudev.py
+++ semana05/dia_2/01-prueba-apiperudev.py
@@ -1,6 +1,8 @@
 import requests
+from decouple import config
+import os 
 
-token = "4e626d7c6d404d06d93001d27842a3ba07c7dff3d0a64a39467e149c77773c2b"
+token = config('API_TOKEN')
 url_dni = 'https://apiperu.dev/api/dni'
 
 dni = input("ingrese dni :")
@@ -10,11 +12,56 @@ data_request = {
 }
 
 headers_request = {
-    "Authorization": "Bearer " + token,
+    "Authorization": "Bearer " + str(token),
     "Content-Type": "application/json"
 }
 
 response = requests.post(url_dni,json=data_request,headers=headers_request)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error {response.status_code}")
+
+
+token = config('API_TOKEN')
+url_ruc = 'https://apiperu.dev/api/ruc'
+
+ruc = input("ingrese ruc :")
+
+data_request_ruc = {
+    "ruc": ruc 
+}
+
+headers_request_ruc = {
+    "Authorization": "Bearer " + str(token),
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url_ruc,json=data_request_ruc,headers=headers_request_ruc)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error {response.status_code}")
+
+
+
+token = config('API_TOKEN')
+url_tipo = ' https://apiperu.dev/api/tipo_de_cambio'
+
+tipo = input("tipo de cambio :")
+
+data_request_tipo = {
+    "tipo_de_cambio": tipo 
+}
+
+headers_request_tipo = {
+    "Authorization": "Bearer " + str(token),
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url_tipo,json=data_request_tipo,headers=headers_request_tipo)
 
 if response.status_code == 200:
     print(response.json())
